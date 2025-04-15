2aabf22eb76f1304408819ac6a455020fee10b0e
carluandraster <andres.efstratiadis@gmail.com>
2025-04-14 21:55:22 -0300

    Modificar la carga alta y baja

--- src/mv.c
+++ src/mv.c
@@ -7,7 +7,8 @@
     Inicializa la máquina virtual, configurando los registros y la tabla de segmentos.
     Se establece el tamaño del segmento de datos y el segmento de código, así como la base
 */
-void inicializar_maquina(t_MV* mv, short int tamano) {
+void inicializar_maquina(t_MV *mv, short int tamano)
+{
     mv->registros[CS] = 0x00000000;
     mv->registros[DS] = 0x00010000;
     mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].base = 0;
@@ -22,7 +23,8 @@ void inicializar_maquina(t_MV* mv, short int tamano) {
     Se lee un array de instrucciones donde cada elemento tiene operacion y los
     operando 1 y 2 con sus respectivos tipo y valor. Este array es generado previamente.
 */
-void ejecutar_maquina(t_MV* mv, t_instruccion* instrucciones, int instruccion_size) {
+void ejecutar_maquina(t_MV *mv, t_instruccion *instrucciones, int instruccion_size)
+{
     t_func0 t_func0[1];
     t_func1 t_func1[10];
     t_func2 t_func2[15];
@@ -32,22 +34,22 @@ void ejecutar_maquina(t_MV* mv, t_instruccion* instrucciones, int instruccion_si
     printf("Ejecutando la maquina virtual...\n");
 
     mv->registros[IP] = mv->registros[CS]; // Inicializa el registro IP con la dirección del segmento de código
-    while ((mv->registros[IP] & 0x0FFFF) < instruccion_size ) {
-        posicion = mv->registros[IP] & 0x0FFFF; // Actualiza la posición en el array de instrucciones
+    while ((mv->registros[IP] & 0x0FFFF) < instruccion_size)
+    {
+        posicion = mv->registros[IP] & 0x0FFFF;                                                       // Actualiza la posición en el array de instrucciones
         mv->registros[IP] += instrucciones[posicion].op1.tipo + instrucciones[posicion].op2.tipo + 1; // Actualiza la posición en el array de instrucciones
 
         if (instrucciones[posicion].op1.tipo == NINGUNO && instrucciones[posicion].op2.tipo == NINGUNO)
             t_func0[(instrucciones[posicion].opcode & 0x01F) - 0x0F](mv);
+        else if (instrucciones[posicion].op1.tipo == NINGUNO)
+            t_func1[(instrucciones[posicion].opcode & 0x01F)](mv, instrucciones[posicion].op2);
         else
-            if (instrucciones[posicion].op1.tipo == NINGUNO)
-                t_func1[(instrucciones[posicion].opcode & 0x01F)](mv, instrucciones[posicion].op2);
-            else
-                t_func2[(instrucciones[posicion].opcode & 0x01F) - 16](mv, instrucciones[posicion].op1, instrucciones[posicion].op2);
+            t_func2[(instrucciones[posicion].opcode & 0x01F) - 16](mv, instrucciones[posicion].op1, instrucciones[posicion].op2);
     }
-    for (int i = 0; i < 12; i++) {
-        printf("[%d]: %02X\n",i,mv->memoria[mv->tabla_segmentos[1].base + i] );
+    for (int i = 0; i < 12; i++)
+    {
+        printf("[%d]: %02X\n", i, mv->memoria[mv->tabla_segmentos[1].base + i]);
     }
-    
 }
 
 /*
@@ -57,12 +59,14 @@ void ejecutar_maquina(t_MV* mv, t_instruccion* instrucciones, int instruccion_si
     Registro -> 1 byte
     Todo esto es parte del archivo leido, es del code segment.
 */
-void valor_operacion(t_operador* op, t_MV mv) {
+void valor_operacion(t_operador *op, t_MV mv)
+{
     short index = (mv.registros[IP] >> 16) & 0x000000FF;      // Extraer el índice del segmento
     short offset = mv.registros[IP] & 0x0000FFFF;             // Extraer el offset
     short dirFisic = mv.tabla_segmentos[index].base + offset; // Calcular la dirección física
     op->valor = 0;                                            // Inicializar el valor del operando a 0
-    switch (op->tipo) {
+    switch (op->tipo)
+    {
     case MEMORIA:
         op->valor = mv.memoria[dirFisic] & 0x0FF;
         op->valor <<= 8;
@@ -92,16 +96,19 @@ void valor_operacion(t_operador* op, t_MV mv) {
     Si el tipo de operando/ sector de registro es invalido se muestra mensaje de error
     y devuelve valor -1.
 */
-int getValor(t_operador op, t_MV maquina) {
+int getValor(t_operador op, t_MV maquina)
+{
     short valor = 0;
-    switch (op.tipo) {
+    switch (op.tipo)
+    {
     case REGISTRO:
     {
         // Extraer el sector del registro( EAX=00, AL=01, AH=10, AX=11)
         short sectorReg = (op.valor & 0x000C) >> 2;
         short codigoReg = (op.valor & 0x00F0) >> 4; // Extraer el registro
-        
-        switch (sectorReg) {
+
+        switch (sectorReg)
+        {
         case 0: // EAX XXXX
             return maquina.registros[codigoReg];
         case 1: // AL 000X
@@ -124,11 +131,13 @@ int getValor(t_operador op, t_MV maquina) {
         short offsetReg = maquina.registros[codigoReg] & 0x0FFFF;
         short offset = (op.valor >> 8) & 0x0FFFF;
         int dirFisic = maquina.tabla_segmentos[(maquina.registros[codigoReg] >> 16) & 0x0000FFFF].base + offsetReg + offset;
-        
+
         if ((dirFisic < maquina.tabla_segmentos[(maquina.registros[DS] >> 16) & 0x0FFFF].base) || ((dirFisic + 4) > maquina.tabla_segmentos[(maquina.registros[DS] >> 16) & 0x0FFFF].tamano))
             error(&maquina, 3); // Error: Overflow de memoria
-        else {
-            for (int i = 0; i < TAM_CELDA; i++) {
+        else
+        {
+            for (int i = 0; i < TAM_CELDA; i++)
+            {
                 valor <<= 8;
                 valor |= (maquina.memoria[dirFisic + i] & 0x000000FF);
             }
@@ -150,19 +159,22 @@ int getValor(t_operador op, t_MV maquina) {
     Dependiendo del tipo de operando (MEMORIA, REGISTRO), se actualiza el valor
     en la memoria o en los registros correspondientes.
 */
-void setValor(t_operador op, int valor, t_MV* maquina) {
-    switch (op.tipo) {
+void setValor(t_operador op, int valor, t_MV *maquina)
+{
+    switch (op.tipo)
+    {
     case MEMORIA:
     {
         short codReg = (op.valor >> 4) & 0x000F;
         short offsetReg = maquina->registros[codReg] & 0x0FFFF;
         short offset = (op.valor >> 8) & 0x0FFFF;
         int dirFisic = maquina->tabla_segmentos[(maquina->registros[codReg] >> 16) & 0x0FFFF].base + offsetReg + offset;
- 
+
         if ((dirFisic < maquina->tabla_segmentos[(maquina->registros[DS] >> 16) & 0x0FFFF].base) || ((dirFisic + 4) > maquina->tabla_segmentos[(maquina->registros[DS] >> 16) & 0x0FFFF].tamano))
             error(maquina, 3); // Error: Overflow de memoria
         else
-            for (int i = 0; i < TAM_CELDA; i++) {
+            for (int i = 0; i < TAM_CELDA; i++)
+            {
                 maquina->memoria[dirFisic + i] = (valor >> ((3 - i) * 8)) & 0x000000FF;
             }
     }
@@ -171,7 +183,8 @@ void setValor(t_operador op, int valor, t_MV* maquina) {
     {
         short sectorReg = (op.valor & 0x000C) >> 2;
         short codigoReg = (op.valor & 0x00F0) >> 4;
-        switch (sectorReg) {
+        switch (sectorReg)
+        {
         case 0: // EAX XXXX
             maquina->registros[codigoReg] = valor;
             break;
@@ -179,7 +192,7 @@ void setValor(t_operador op, int valor, t_MV* maquina) {
             maquina->registros[codigoReg] = (maquina->registros[codigoReg] & 0xFFFFFF00) | (valor & 0x000000FF);
             break;
         case 2: // AH 00X0
-            maquina->registros[codigoReg] = (maquina->registros[codigoReg] & 0xFFFFFF00FF) | ((valor << 8)& 0x0000FF00);
+            maquina->registros[codigoReg] = (maquina->registros[codigoReg] & 0xFFFFFF00FF) | ((valor << 8) & 0x0000FF00);
             break;
         case 3: // AX 00XX
             maquina->registros[codigoReg] = (maquina->registros[codigoReg] & 0xFFFF0000) | (valor & 0x0000FFFF);
@@ -200,8 +213,10 @@ void setValor(t_operador op, int valor, t_MV* maquina) {
     Dependiendo del código de error, se muestra un mensaje específico y se ajusta el registro IP
     al final del codigo del code segment para finalizarlo.
 */
-void error(t_MV* mv, int errorCode) {
-    switch (errorCode) {
+void error(t_MV *mv, int errorCode)
+{
+    switch (errorCode)
+    {
     case 1:
         printf("Error: Operacion no valida.\n");
         break;
@@ -221,30 +236,33 @@ void error(t_MV* mv, int errorCode) {
     Este array contiene por cada elemento, el opcode y los operandos (tipo y valor)
     para luego recorrerlo y ejecutar cada instruccion.
 */
-void genero_array_instrucciones(t_MV* mv, t_instruccion** instrucciones, int* instruccion_size) {
+void genero_array_instrucciones(t_MV *mv, t_instruccion **instrucciones, int *instruccion_size)
+{
     // obtengo el tamaño del segmento de código
     short size_code = mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].tamano - mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].base;
     // Asigna memoria para las instrucciones
     *instrucciones = malloc(sizeof(t_instruccion) * size_code);
 
-    if (*instrucciones == NULL) {
+    if (*instrucciones == NULL)
+    {
         printf("Error al asignar memoria para el disassembler\n");
         return;
     }
-    else {
+    else
+    {
         short index = (mv->registros[IP] >> 16) & 0x0FFFF;                                                                           // Extraer el índice del segmento
         short offset = mv->registros[IP] & 0x0FFFF;                                                                                  // Extraer el offset
         short dirFisic = mv->tabla_segmentos[index].base + offset;                                                                   // Calcular la dirección física
         *instruccion_size = mv->tabla_segmentos[mv->registros[CS] >> 16].tamano - mv->tabla_segmentos[mv->registros[CS] >> 16].base; // Tamaño del segmento de código
 
-        while (dirFisic < *instruccion_size) {
+        while (dirFisic < *instruccion_size)
+        {
             // Leer la instrucción de la memoria
             char instruccion = mv->memoria[dirFisic];
 
             int posicion = mv->registros[IP] & 0x0FFFF; // Obtener la posición de la instrucción en el array
 
             mv->registros[IP]++;
-
 
             (*instrucciones)[posicion].opcode = instruccion & 0x0FF; // Guardar el opcode en la instrucción actual
             // Ejecutar la instrucción
@@ -260,39 +278,5 @@ void genero_array_instrucciones(t_MV* mv, t_instruccion** instrucciones, int* in
             offset = mv->registros[IP] & 0x0FFFF;
             dirFisic = mv->tabla_segmentos[index].base + offset;
         }
-    }
-}
-
-void cargaAlta(t_operador op, short int valor, t_MV* maquina) {
-    switch (op.tipo) {
-        // 1 registro = 4 bytes
-    case REGISTRO:
-        setValor(op, (getValor(op, *maquina) & 0x0000FFFF) | (valor << 16), maquina);
-        break;
-        // 1 celda de memoria = 1 byte
-    case MEMORIA:
-        setValor(op, (getValor(op, *maquina) & 0xF) | (valor << 4), maquina);
-        break;
-
-    default:
-        printf("Error: tipo de operando invalido");
-        break;
-    }
-}
-
-void cargaBaja(t_operador op, short int valor, t_MV* maquina) {
-    switch (op.tipo) {
-        // 1 registro = 4 bytes
-    case REGISTRO:
-        setValor(op, (getValor(op, *maquina) & 0xFFFF0000) | valor, maquina);
-        break;
-        // 1 celda de memoria = 1 byte
-    case MEMORIA:
-        setValor(op, (getValor(op, *maquina) & 0xF0) | valor & 0xF, maquina);
-        break;
-
-    default:
-        printf("Error: tipo de operando invalido");
-        break;
     }
 }
--- src/operaciones.c
+++ src/operaciones.c
@@ -9,12 +9,14 @@
 // Con int no pasa.
 // Hay valores negativos en inmediatos y offset
 
-void FuncionCC(t_MV* maquina, int resultado) {                               // actualiza CC
+void FuncionCC(t_MV *maquina, int resultado)
+{                               // actualiza CC
     maquina->registros[CC] = 0; // reseteo CC
 
     if (resultado == 0)
         maquina->registros[CC] = 0x40000000;
-    else {
+    else
+    {
         if (resultado < 0)
             maquina->registros[CC] = 0x80000000;
         else
@@ -22,11 +24,14 @@ void FuncionCC(t_MV* maquina, int resultado) {                               // 
     }
 }
 
-int deBinarioStringAInt(char bin[]) {
+int deBinarioStringAInt(char bin[])
+{
     int exponente = 0;
     int resultado = 0;
-    for (int i = strlen(bin) - 1; i >= 0; i--) {
-        if (bin[i] != '0' && bin[i] != '1') {
+    for (int i = strlen(bin) - 1; i >= 0; i--)
+    {
+        if (bin[i] != '0' && bin[i] != '1')
+        {
             printf("ERROR: se pidio un numero binario y se ingreso cualquier cosa");
             break;
         }
@@ -36,20 +41,24 @@ int deBinarioStringAInt(char bin[]) {
     return resultado;
 }
 
-void deIntABinarioString(int nro, char bin[]) {
+void deIntABinarioString(int nro, char bin[])
+{
     bin[32] = '\0';
-    for (int i = 31; i >= 0; i--) {
+    for (int i = 31; i >= 0; i--)
+    {
         bin[i] = '0' + (nro & 0x1);
         nro = nro >> 1;
     }
 }
 
-void MOV(t_MV* maquina, t_operador op1, t_operador op2) {
+void MOV(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando MOV...\n");
     int b = getValor(op2, *maquina);
     setValor(op1, b, maquina);
 }
-void ADD(t_MV* maquina, t_operador op1, t_operador op2) {
+void ADD(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando ADD...\n");
     int a = getValor(op1, *maquina);
     int b = getValor(op2, *maquina);
@@ -57,7 +66,8 @@ void ADD(t_MV* maquina, t_operador op1, t_operador op2) {
     printf("Resultado ADD: %d\n", b + a);
     FuncionCC(maquina, b + a);
 }
-void SUB(t_MV* maquina, t_operador op1, t_operador op2) {
+void SUB(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando SUB...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -65,14 +75,16 @@ void SUB(t_MV* maquina, t_operador op1, t_operador op2) {
     printf("Resultado: %d\n", b - a);
     FuncionCC(maquina, b - a);
 }
-void SWAP(t_MV* maquina, t_operador op1, t_operador op2) {
+void SWAP(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando SWAP...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
     setValor(op1, a, maquina);
     setValor(op2, b, maquina);
 }
-void MUL(t_MV* maquina, t_operador op1, t_operador op2) {
+void MUL(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando MUL...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -80,11 +92,13 @@ void MUL(t_MV* maquina, t_operador op1, t_operador op2) {
     printf("Resultado: %d\n", b * a);
     FuncionCC(maquina, b * a);
 }
-void DIV(t_MV* maquina, t_operador op1, t_operador op2) {
+void DIV(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando DIV...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
-    if (a != 0) {
+    if (a != 0)
+    {
         setValor(op1, b / a, maquina);
         maquina->registros[AC] = b % a; // resto
         printf("Resultado: cosciente %d resto %d\n", b / a, b % a);
@@ -93,7 +107,8 @@ void DIV(t_MV* maquina, t_operador op1, t_operador op2) {
     else
         error(maquina, 2);
 }
-void CMP(t_MV* maquina, t_operador op1, t_operador op2) {
+void CMP(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando CMP...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -101,7 +116,8 @@ void CMP(t_MV* maquina, t_operador op1, t_operador op2) {
     FuncionCC(maquina, resto);
     printf("Resultado de la comparacion: %d (CC = %d)\n", resto, maquina->registros[CC]);
 }
-void SHL(t_MV* maquina, t_operador op1, t_operador op2) {
+void SHL(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando SHL...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -110,7 +126,8 @@ void SHL(t_MV* maquina, t_operador op1, t_operador op2) {
     FuncionCC(maquina, resultado);
     printf("Resultado: %d\n", resultado);
 }
-void SHR(t_MV* maquina, t_operador op1, t_operador op2) {
+void SHR(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando SHR...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -119,7 +136,8 @@ void SHR(t_MV* maquina, t_operador op1, t_operador op2) {
     FuncionCC(maquina, resultado);
     printf("Resultado: %d\n", resultado);
 }
-void AND(t_MV* maquina, t_operador op1, t_operador op2) {
+void AND(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando AND...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -128,7 +146,8 @@ void AND(t_MV* maquina, t_operador op1, t_operador op2) {
     FuncionCC(maquina, resultado);
     printf("Resultado: %d (0x%X)\n", resultado, resultado);
 }
-void OR(t_MV* maquina, t_operador op1, t_operador op2) {
+void OR(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando OR...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -137,7 +156,8 @@ void OR(t_MV* maquina, t_operador op1, t_operador op2) {
     FuncionCC(maquina, resultado);
     printf("Resultado: %d (0x%X)\n", resultado, resultado);
 }
-void XOR(t_MV* maquina, t_operador op1, t_operador op2) {
+void XOR(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando XOR...\n");
     int b = getValor(op1, *maquina);
     int a = getValor(op2, *maquina);
@@ -146,34 +166,41 @@ void XOR(t_MV* maquina, t_operador op1, t_operador op2) {
     FuncionCC(maquina, resultado);
     printf("Resultado: %d (0x%X)\n", resultado, resultado);
 }
-void LDL(t_MV* maquina, t_operador op1, t_operador op2) {
+void LDL(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando LDL...\n");
-    cargaBaja(op1, getValor(op2, *maquina), maquina);
+    setValor(op1, (getValor(op1, *maquina) & 0xFFFF0000) | getValor(op2, *maquina), maquina);
 }
-void LDH(t_MV* maquina, t_operador op1, t_operador op2) {
+void LDH(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando LDH...\n");
-    cargaAlta(op1, getValor(op2, *maquina), maquina);
+    setValor(op1, (getValor(op1, *maquina) & 0x0000FFFF) | (getValor(op2, *maquina) << 16), maquina);
 }
-void RND(t_MV* maquina, t_operador op1, t_operador op2) {
+void RND(t_MV *maquina, t_operador op1, t_operador op2)
+{
     printf("Ejecutando RND...\n");
 
     int a = getValor(op2, *maquina);
 
     static int iniciado = 0;
-    if (!iniciado) {
+    if (!iniciado)
+    {
         srand(time(NULL)); // Inicializar el generador de números aleatorios solo una vez
         iniciado = 1;
     }
-  if(a > 0){
-    int resultado = rand() % (a + 1);
-    setValor(op1, resultado, maquina);
-    printf("Resultado: %d\n", resultado);}
-  else
-    printf("RDN no admite negativos\n");   
+    if (a > 0)
+    {
+        int resultado = rand() % (a + 1);
+        setValor(op1, resultado, maquina);
+        printf("Resultado: %d\n", resultado);
+    }
+    else
+        printf("RDN no admite negativos\n");
 }
 
-void SYS(t_MV* maquina, t_operador op1) {
-    char bin[33], * str;
+void SYS(t_MV *maquina, t_operador op1)
+{
+    char bin[33], *str;
     int CL = maquina->registros[C] & 0xFF;
     int CH = (maquina->registros[C] >> 8) & 0xFF;
     int i, j, k, x, dirFisica;
@@ -184,13 +211,15 @@ void SYS(t_MV* maquina, t_operador op1) {
     getSalidas(splitter1, salidas);
     setTamanio(&splitter2, 8);
     dirFisica = maquina->tabla_segmentos[1].base + maquina->registros[D] & 0xFFFF;
-    switch (op1.valor) {
+    switch (op1.valor)
+    {
     case 1: // Modo lectura
 
         switch (maquina->registros[A] & 0xFF) // AL
         {
         case 1: // Leer en decimal
-            for (i = 0; i < CL; i++) {
+            for (i = 0; i < CL; i++)
+            {
                 printf("[%04X]: ", dirFisica + i * CH);
                 scanf("%d", &x);
                 setEntrada(&splitter2, x);
@@ -200,7 +229,8 @@ void SYS(t_MV* maquina, t_operador op1) {
             }
             break;
         case 2: // Leer caracter
-            for (i = 0; i < CL; i++) {
+            for (i = 0; i < CL; i++)
+            {
                 printf("[%04X]: ", dirFisica + i * CH);
                 scanf("%s", str);
                 for (j = 0; j < CH; j++)
@@ -208,7 +238,8 @@ void SYS(t_MV* maquina, t_operador op1) {
             }
             break;
         case 4: // Leer en octal
-            for (i = 0; i < CL; i++) {
+            for (i = 0; i < CL; i++)
+            {
                 printf("[%04X]: ", dirFisica + i * CH);
                 scanf("%o", &x);
                 setEntrada(&splitter2, x);
@@ -218,17 +249,20 @@ void SYS(t_MV* maquina, t_operador op1) {
             }
             break;
         case 8: // Leer en hexadecimal
-            for (i = 0; i < CL; i++) {
+            for (i = 0; i < CL; i++)
+            {
                 printf("[%4X]: ", dirFisica + i * CH);
                 scanf("%x", &x);
-                for (j = CH - 1; j >= 0; j--) {
+                for (j = CH - 1; j >= 0; j--)
+                {
                     maquina->memoria[dirFisica + i * CH + j] = x & 0xFF;
                     x = x >> 8;
                 }
             }
             break;
         case 16: // Leer en binario
-            for (i = 0; i < CL; i++) {
+            for (i = 0; i < CL; i++)
+            {
                 printf("[%04X]: ", dirFisica + i * CH);
                 scanf("%s", bin);
                 x = deBinarioStringAInt(bin);
@@ -245,14 +279,17 @@ void SYS(t_MV* maquina, t_operador op1) {
 
         break;
     case 2: // Modo escritura
-        for (i = 0; i < CL; i++) {
+        for (i = 0; i < CL; i++)
+        {
             printf("[%04X]: ", dirFisica + i * CH);
             for (k = 4; k >= 0; k--)
                 if (salidas[k])
-                    switch (k) {
+                    switch (k)
+                    {
                     case 4: // Escribe binario
                         printf("0b");
-                        for (j = 0; j < CH; j++) {
+                        for (j = 0; j < CH; j++)
+                        {
                             deIntABinarioString(maquina->memoria[dirFisica + i * CH + j], bin);
                             printf("%s", bin);
                         }
@@ -267,7 +304,8 @@ void SYS(t_MV* maquina, t_operador op1) {
                     case 2: // Escribe en octal
                         printf("0o");
                         x = 0;
-                        for (j = 0; j < CH; j++) {
+                        for (j = 0; j < CH; j++)
+                        {
                             x = x << 8;
                             x = x | maquina->memoria[dirFisica + i * CH + j];
                         }
@@ -283,7 +321,8 @@ void SYS(t_MV* maquina, t_operador op1) {
                         break;
                     case 0: // Escribe decimal
                         x = 0;
-                        for (j = 0; j < CH; j++) {
+                        for (j = 0; j < CH; j++)
+                        {
                             x = x << 8;
                             x = x | maquina->memoria[dirFisica + i * CH + j];
                         }
@@ -297,7 +336,8 @@ void SYS(t_MV* maquina, t_operador op1) {
         }
     }
 }
-void Salto(t_MV* mv, t_operador op1) {                                                                                      // FUNCION QUE EJECUTA EL SALTO DE TODAS LAS FUNCIONES JUMP
+void Salto(t_MV *mv, t_operador op1)
+{                                                                                      // FUNCION QUE EJECUTA EL SALTO DE TODAS LAS FUNCIONES JUMP
     int valor = getValor(op1, *mv);                                                    // El valor del operando es la direccion logica a la que queremos saltar         // Extraer offset (los 16 bits bajos)
     short pos = mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].base + valor; // Verificar que esté dentro del segmento de codigo
     short size = mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].tamano - mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].base;
@@ -305,19 +345,22 @@ void Salto(t_MV* mv, t_operador op1) {                                          
 
     if (pos > size)
         error(mv, 3);
-    else {
+    else
+    {
         mv->registros[IP] = pos;
         printf("Salto a direccion : 0x%04X\n", pos);
     }
 }
-void JMP(t_MV* maquina, t_operador op1) {
+void JMP(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando JMP...\n");
     if (op1.tipo != INMEDIATO && op1.tipo != MEMORIA)
         printf("Error: JMP solo admite inmediatos o direcciones logicas (tipo MEMORIA)\n");
     else
         Salto(maquina, op1);
 }
-void JZ(t_MV* maquina, t_operador op1) {
+void JZ(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando JZ...\n");
 
     if (maquina->registros[CC] & 0x40000000)
@@ -325,42 +368,48 @@ void JZ(t_MV* maquina, t_operador op1) {
     else
         printf("No se cumple la condicion para el salto\n");
 }
-void JP(t_MV* maquina, t_operador op1) {
+void JP(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando JP...\n");
     if (maquina->registros[CC] == 0)
         Salto(maquina, op1);
     else
         printf("No se cumple la condicion para el salto\n");
 }
-void JN(t_MV* maquina, t_operador op1) {
+void JN(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando JN...\n");
     if (maquina->registros[CC] & 0x80000000)
         Salto(maquina, op1);
     else
         printf("No se cumple la condicion para el salto\n");
 }
-void JNZ(t_MV* maquina, t_operador op1) {
+void JNZ(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando JNZ...\n");
     if (!(maquina->registros[CC] & 0x40000000))
         Salto(maquina, op1);
     else
         printf("No se cumple la condicion para el salto\n");
 }
-void JNP(t_MV* maquina, t_operador op1) {
+void JNP(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando JNP...\n");
     if (!(maquina->registros[CC] == 0))
         Salto(maquina, op1);
     else
         printf("No se cumple la condicion para el salto\n");
 }
-void JNN(t_MV* maquina, t_operador op1) {
+void JNN(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando JNN...\n");
     if (!(maquina->registros[CC] & 0x80000000))
         Salto(maquina, op1);
     else
         printf("No se cumple la condicion para el salto\n");
 }
-void NOT(t_MV* maquina, t_operador op1) {
+void NOT(t_MV *maquina, t_operador op1)
+{
     printf("Ejecutando NOT...\n");
 
     int b = getValor(op1, *maquina);
@@ -371,12 +420,14 @@ void NOT(t_MV* maquina, t_operador op1) {
     printf("Resultado: %d (0x%X)\n", resultado, resultado);
 }
 
-void STOP(t_MV* mv) {
+void STOP(t_MV *mv)
+{
     printf("Ejecutando STOP...\n");
     exit(1);
 }
 
-void inicializo_vector_op(t_func0 func0[], t_func1 func1[], t_func2 func2[]) {
+void inicializo_vector_op(t_func0 func0[], t_func1 func1[], t_func2 func2[])
+{
     func0[0] = &STOP;
 
     func1[0] = &SYS;
