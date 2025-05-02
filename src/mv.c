8a337a0f7f24244b7812fccf00e71556f0ca6ca9
carluandraster <andres.efstratiadis@gmail.com>
2025-05-02 16:43:18 -0300

    Update mv.c
    Nuevos errores
    4: memoria insuficiente
    5: stack overflow
    6: stack underflow

--- src/mv.c
+++ src/mv.c
@@ -7,7 +7,8 @@
     Inicializa la máquina virtual, configurando los registros y la tabla de segmentos.
     Se establece el tamaño del segmento de datos y el segmento de código, así como la base
 */
-void inicializar_maquina(t_MV* mv, short int tamano) {
+void inicializar_maquina(t_MV *mv, short int tamano)
+{
     mv->registros[CS] = 0x00000000;
     mv->registros[DS] = 0x00010000;
     mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].base = 0;
@@ -22,21 +23,22 @@ void inicializar_maquina(t_MV* mv, short int tamano) {
     Se lee un array de instrucciones donde cada elemento tiene operacion y los
     operando 1 y 2 con sus respectivos tipo y valor. Este array es generado previamente.
 */
-void ejecutar_maquina(t_MV* mv, t_instruccion* instrucciones, int instruccion_size) {
+void ejecutar_maquina(t_MV *mv, t_instruccion *instrucciones, int instruccion_size)
+{
     t_func0 t_func0[1];
     t_func1 t_func1[10];
     t_func2 t_func2[15];
     int posicion = 0;
-    
-    
+
     inicializo_vector_op(t_func0, t_func1, t_func2); // Inicializa los vectores de funciones
-    mv->registros[IP] = mv->registros[CS]; // Inicializa el registro IP con la dirección del segmento de código
+    mv->registros[IP] = mv->registros[CS];           // Inicializa el registro IP con la dirección del segmento de código
     mv->flag_ejecucion = 1;
-    
-    while (mv->flag_ejecucion && mv->registros[IP] < instruccion_size) {
+
+    while (mv->flag_ejecucion && mv->registros[IP] < instruccion_size)
+    {
         posicion = mv->registros[IP] & 0x0FFFF;
         mv->registros[IP] += instrucciones[posicion].op1.tipo + instrucciones[posicion].op2.tipo + 1; // Actualiza la posición en el array de instrucciones
-    
+
         codOperacionValido((instrucciones[posicion].opcode & 0x01F), *mv);
         if (instrucciones[posicion].op1.tipo == NINGUNO && instrucciones[posicion].op2.tipo == NINGUNO)
             t_func0[(instrucciones[posicion].opcode & 0x01F) - 0x0F](mv);
@@ -45,7 +47,6 @@ void ejecutar_maquina(t_MV* mv, t_instruccion* instrucciones, int instruccion_si
         else
             t_func2[(instrucciones[posicion].opcode & 0x01F) - 16](mv, instrucciones[posicion].op1, instrucciones[posicion].op2);
     }
-   
 }
 
 /*
@@ -55,12 +56,14 @@ void ejecutar_maquina(t_MV* mv, t_instruccion* instrucciones, int instruccion_si
     Registro -> 1 byte
     Todo esto es parte del archivo leido, es del code segment.
 */
-void valor_operacion(t_operador* op, t_MV mv) {
+void valor_operacion(t_operador *op, t_MV mv)
+{
     short index = (mv.registros[IP] >> 16) & 0x000000FF;      // Extraer el índice del segmento
     short offset = mv.registros[IP] & 0x0000FFFF;             // Extraer el offset
     short dirFisic = mv.tabla_segmentos[index].base + offset; // Calcular la dirección física
     op->valor = 0;                                            // Inicializar el valor del operando a 0
-    switch (op->tipo) {
+    switch (op->tipo)
+    {
     case MEMORIA:
         op->valor = mv.memoria[dirFisic] & 0x0FF;
         op->valor <<= 8;
@@ -90,16 +93,19 @@ void valor_operacion(t_operador* op, t_MV mv) {
     Si el tipo de operando/ sector de registro es invalido se muestra mensaje de error
     y devuelve valor -1.
 */
-int getValor(t_operador op, t_MV maquina) {
+int getValor(t_operador op, t_MV maquina)
+{
     int valor = 0;
-    switch (op.tipo) {
+    switch (op.tipo)
+    {
     case REGISTRO:
     {
         // Extraer el sector del registro( EAX=00, AL=01, AH=10, AX=11)
         short sectorReg = (op.valor & 0x000C) >> 2;
         short codigoReg = (op.valor & 0x00F0) >> 4; // Extraer el registro
 
-        switch (sectorReg) {
+        switch (sectorReg)
+        {
         case 0: // EAX XXXX
             return maquina.registros[codigoReg];
         case 1: // AL 000X
@@ -112,7 +118,7 @@ int getValor(t_operador op, t_MV maquina) {
             valor = (maquina.registros[codigoReg] & 0x00FFFF) << 16;
             return valor >> 16;
         default:
-        printf("op:%d valor:%06X\n",op.tipo,op.valor);      
+            printf("op:%d valor:%06X\n", op.tipo, op.valor);
             printf("Tipo de sector de registro invalido. [getValor()] EAX/AX/AL/AH\n");
             break;
         }
@@ -129,8 +135,10 @@ int getValor(t_operador op, t_MV maquina) {
 
         if ((dirFisic < maquina.tabla_segmentos[(maquina.registros[DS] >> 16) & 0x0FFFF].base) || ((dirFisic + 4) > maquina.tabla_segmentos[(maquina.registros[DS] >> 16) & 0x0FFFF].tamano))
             error(&maquina, 3); // Error: Overflow de memoria
-        else {
-            for (int i = 0; i < TAM_CELDA; i++) {
+        else
+        {
+            for (int i = 0; i < TAM_CELDA; i++)
+            {
                 valor <<= 8;
                 valor |= (maquina.memoria[dirFisic + i] & 0x000000FF);
             }
@@ -152,8 +160,10 @@ int getValor(t_operador op, t_MV maquina) {
     Dependiendo del tipo de operando (MEMORIA, REGISTRO), se actualiza el valor
     en la memoria o en los registros correspondientes.
 */
-void setValor(t_operador op, int valor, t_MV* maquina) {
-    switch (op.tipo) {
+void setValor(t_operador op, int valor, t_MV *maquina)
+{
+    switch (op.tipo)
+    {
     case MEMORIA:
     {
         short codReg = (op.valor >> 4) & 0x000F;
@@ -164,7 +174,8 @@ void setValor(t_operador op, int valor, t_MV* maquina) {
         if ((dirFisic < maquina->tabla_segmentos[(maquina->registros[DS] >> 16) & 0x0FFFF].base) || ((dirFisic + 4) > maquina->tabla_segmentos[(maquina->registros[DS] >> 16) & 0x0FFFF].tamano))
             error(maquina, 3); // Error: Overflow de memoria
         else
-            for (int i = 0; i < TAM_CELDA; i++) {
+            for (int i = 0; i < TAM_CELDA; i++)
+            {
                 maquina->memoria[dirFisic + i] = (valor >> ((3 - i) * 8)) & 0x000000FF;
             }
     }
@@ -173,7 +184,8 @@ void setValor(t_operador op, int valor, t_MV* maquina) {
     {
         short sectorReg = (op.valor & 0x000C) >> 2;
         short codigoReg = (op.valor & 0x00F0) >> 4;
-        switch (sectorReg) {
+        switch (sectorReg)
+        {
         case 0: // EAX XXXX
             maquina->registros[codigoReg] = valor;
             break;
@@ -199,7 +211,8 @@ void setValor(t_operador op, int valor, t_MV* maquina) {
     Verifica si el codigo de operacion se encuentra dentro de los disponibles
     En caso de que no se encuentre se lanza -> Error: Operacion no valida y se corta la ejecucion
 */
-void codOperacionValido(int cod_op, t_MV mv) {
+void codOperacionValido(int cod_op, t_MV mv)
+{
     if (!((cod_op >= 0x10 && cod_op <= 0x1E) || (cod_op >= 0x00 && cod_op <= 0x08) || cod_op == 0x0F))
         error(&mv, 1);
 }
@@ -209,17 +222,27 @@ void codOperacionValido(int cod_op, t_MV mv) {
     Dependiendo del código de error, se muestra un mensaje específico y se ajusta el registro IP
     al final del codigo del code segment para finalizarlo.
 */
-void error(t_MV* mv, int errorCode) {
-    switch (errorCode) {
-        case 1:
-            printf("Error: Operacion no valida.\n");
-            break;
-        case 2:
-            printf("Error: Division por cero.\n");
-            break;
-        case 3:
-            printf("Error: Falla de segmento.\n");
-            break;
+void error(t_MV *mv, int errorCode)
+{
+    switch (errorCode)
+    {
+    case 1:
+        printf("Error: Operacion no valida.\n");
+        break;
+    case 2:
+        printf("Error: Division por cero.\n");
+        break;
+    case 3:
+        printf("Error: Falla de segmento.\n");
+        break;
+    case 4:
+        printf("Error: Memoria insuficiente\n");
+        break;
+    case 5:
+        printf("Error: Stack overflow\n");
+        break;
+    case 6:
+        printf("Error: Stack underflow\n");
     }
     mv->flag_ejecucion = 0;
 }
@@ -230,23 +253,27 @@ void error(t_MV* mv, int errorCode) {
     Este array contiene por cada elemento, el opcode y los operandos (tipo y valor)
     para luego recorrerlo y ejecutar cada instruccion.
 */
-void genero_array_instrucciones(t_MV* mv, t_instruccion** instrucciones, int* instruccion_size) {
+void genero_array_instrucciones(t_MV *mv, t_instruccion **instrucciones, int *instruccion_size)
+{
     // obtengo el tamaño del segmento de código
     short size_code = mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].tamano - mv->tabla_segmentos[(mv->registros[CS] >> 16) & 0x0FFFF].base;
     // Asigna memoria para las instrucciones
     *instrucciones = malloc(sizeof(t_instruccion) * size_code);
 
-    if (*instrucciones == NULL) {
+    if (*instrucciones == NULL)
+    {
         printf("Error al asignar memoria para el disassembler\n");
         return;
     }
-    else {
+    else
+    {
         short index = (mv->registros[IP] >> 16) & 0x0FFFF;                                                                           // Extraer el índice del segmento
         short offset = mv->registros[IP] & 0x0FFFF;                                                                                  // Extraer el offset
         short dirFisic = mv->tabla_segmentos[index].base + offset;                                                                   // Calcular la dirección física
         *instruccion_size = mv->tabla_segmentos[mv->registros[CS] >> 16].tamano - mv->tabla_segmentos[mv->registros[CS] >> 16].base; // Tamaño del segmento de código
 
-        while (dirFisic < *instruccion_size) {
+        while (dirFisic < *instruccion_size)
+        {
             // Leer la instrucción de la memoria
             char instruccion = mv->memoria[dirFisic];
 
