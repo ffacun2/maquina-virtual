583307ce0f9173e35c4cc6258b57abff983756bc
carluandraster <andres.efstratiadis@gmail.com>
2025-05-03 08:44:50 -0300

    Update mv.c

--- src/mv.c
+++ src/mv.c
@@ -67,6 +67,8 @@ void inicializar_maquina2(t_MV *mv, short int tamanoCS, short int tamanoDS, shor
             mv->tabla_segmentos[i].tamano = V[i];
         }
     }
+
+    mv->registros[IP] = mv->registros[CS] + offsetEntryPoint;
 }
 
 void inicializo_registros(t_MV *mv, int registros[])
