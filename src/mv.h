a3cc8dc3addf05dd25c01b947a1c7e1d1668c957
Facundo Criado <facu-1999-44@hotmail.com>
2025-05-02 21:02:57 -0300

    #Modificaciones - Lectura vmi - Validacion de header vmx, vmi - Lectura de comandos de consola - Inicializacion con vmi -

--- src/mv.h
+++ src/mv.h
@@ -10,11 +10,11 @@
 #define TAMANO_MEMORIA 16384
 #define CANT_SEGMENTOS 8
 #define IDENTIFICADOR "VMX25\0"
-#define VERSION_MV 1
+#define VERSION_MV1 1
+#define VERSION_MV2 2
 #define TAM_CELDA 4
 
-enum
-{
+enum {
     // Segmentos
 
     // Code Segment
@@ -53,49 +53,53 @@ enum
     F = 15
 };
 
-typedef struct
-{
+typedef struct {
     short int base;
     short int tamano;
 } t_tabla_segmento;
 
-typedef struct MV
-{
+typedef struct MV {
     char memoria[TAMANO_MEMORIA];
     int registros[CANT_REGISTROS];
     t_tabla_segmento tabla_segmentos[CANT_SEGMENTOS];
     int flag_ejecucion;
+    const char* nombreVMI;
+    const char* nombreVMX;
+    int flag_d;
+    int memory_size;
+    int offsetEntryPoint;
 } t_MV;
 
-typedef enum
-{
+typedef enum {
     NINGUNO = 0,
     REGISTRO = 1,
     INMEDIATO = 2,
     MEMORIA = 3
 } t_operando;
 
-typedef struct
-{
+typedef struct {
     int valor;
     t_operando tipo;
 } t_operador;
 
-typedef struct
-{
+typedef struct {
     char opcode;
     t_operador op1;
     t_operador op2;
 } t_instruccion;
 
-void inicializar_maquina(t_MV *MV, short int tamano);
-void inicializar_maquina2(t_MV *mv, short int tamanoCS, short int tamanoDS, short int tamanoES, short int tamanoSS, short int tamanoKS, short int offsetEntryPoint);
-void ejecutar_maquina(t_MV *maquina, t_instruccion *, int instruccion_size);
-void valor_operacion(t_operador *op, t_MV mv);
+void inicializar_maquina(t_MV* MV, short int tamano);
+void inicializar_maquina2(t_MV* mv, short int tamanoCS, short int tamanoDS, short int tamanoES, short int tamanoSS, short int tamanoKS, short int offsetEntryPoint);
+void inicializo_registros(t_MV* mv, int registros[]);
+void inicializo_segmentos(t_MV* mv, int segmentos[]);
+void inicializo_memoria(t_MV* mv, char memoria[], int size);
+
+void ejecutar_maquina(t_MV* maquina, t_instruccion*, int instruccion_size);
+void valor_operacion(t_operador* op, t_MV mv);
 int getValor(t_operador op, t_MV mv);
-void setValor(t_operador op, int valor, t_MV *maquina);
+void setValor(t_operador op, int valor, t_MV* maquina);
 void codOperacionValido(int cod_op, t_MV mv);
-void error(t_MV *mv, int errorCode);
-void genero_array_instrucciones(t_MV *mv, t_instruccion **instrucciones, int *instruccion_size);
+void error(t_MV* mv, int errorCode);
+void genero_array_instrucciones(t_MV* mv, t_instruccion** instrucciones, int* instruccion_size);
 
 #endif // MV_H_INCLUDED
