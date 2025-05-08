52a9a9fbd027708dd93bb2396020ccba8ef56433
FaustoFi77 <faustofinocchio77@gmail.com>
2025-05-08 17:06:38 -0300

    Update generador_imagen.h

--- src/generador_imagen.h
+++ src/generador_imagen.h
@@ -1,11 +1,3 @@
 #include "mv.h"
 
-typedef struct
-{
-    char header[8];
-    int registros[CANT_REGISTROS];
-    t_tabla_segmento tabla_segmentos[CANT_SEGMENTOS];
-    char memoria[TAMANO_MEMORIA];
-} t_imagen;
-
-t_imagen generarImagen(t_MV mv);
+void generarImagen(t_MV *mv)
